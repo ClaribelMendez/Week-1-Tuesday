7733a6fffd3427fd428d852efc50c5a30cf8c97a
ClaribelMendez <>
2022-01-18 18:26:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Reverse the provided string.
+// Your result must be a string.
+//TEST Cases
+// reverseString("hello") ---> "olleh"
+// reverseString("Howdy") ---> "ydwoH"
+// reverseString("Greetings from Techtonica") ---> "acinothceT morf sgniteerG"
+
+// Bonus points
+// Can you think about edge cases? 
